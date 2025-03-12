0fe2527926da4bf2466f560e04b5e5835687d121
Dương Thuận Tri <trithuanduong123@gmail.com>
2025-03-12 15:03:28 +0700

    Delete bmttnc-hutech-2280603347_buoi2/bmttnc-hutech-2280603347 directory

--- bmttnc-hutech-2280603347_buoi2/bmttnc-hutech-2280603347/TH01/lab01.py
+++
@@ -1,4 +0,0 @@
-tuoi = int(input('Nhap tuoi cua ban: '))
-print('Tuoi cua ban la:', tuoi)
-ten = input('Nhap ten cua ban: ')
-print('Ten cua ban la:', ten)
--- bmttnc-hutech-2280603347_buoi2/bmttnc-hutech-2280603347/TH01/lab10.py
+++
@@ -1,5 +0,0 @@
-def daonguocchuoi(chuoi):
-    return chuoi[::-1]
-
-input_string = input('Nhập một chuỗi: ')
-print('Chuỗi đảo ngược:', daonguocchuoi(input_string))
