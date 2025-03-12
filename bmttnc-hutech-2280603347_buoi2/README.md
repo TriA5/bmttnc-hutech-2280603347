235debdd4205984dcec4e6e8226c19a78d52d9e6
Dương Thuận Tri <trithuanduong123@gmail.com>
2025-03-12 14:18:21 +0700

    Add files via upload

---
+++ bmttnc-hutech-2280603347_buoi2/README.md
@@ -0,0 +1,2 @@
+# bmttnc-hutech-2280603347
+Duong_Thuan_Tri_2280603347
---
+++ bmttnc-hutech-2280603347_buoi2/bmttnc-hutech-2280603347/TH01/lab10.py
@@ -0,0 +1,5 @@
+def daonguocchuoi(chuoi):
+    return chuoi[::-1]
+
+input_string = input('Nhập một chuỗi: ')
+print('Chuỗi đảo ngược:', daonguocchuoi(input_string))
